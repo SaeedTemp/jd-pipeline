bda7da31f96512d881affd5f1a7fc41168e57eb0
nameTemp <125341398+SaeedTemp@users.noreply.github.com>
2023-05-08 23:55:04 +0530

    update

--- main.py
+++ main.py
@@ -81,7 +81,7 @@ def app():
     
     if st.session_state["logged"] == None:
         with container.container():
-            st.title('Bogin')
+            st.title('Login')
     
             # Add form inputs for username and password
             username = st.text_input('Username')
